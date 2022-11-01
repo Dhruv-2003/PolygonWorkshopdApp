9a094bbb05e8aa7355c1a13ae8a6cdbcb6fc72ba
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-11-01 20:06:15 +0530

    dApp created

---
+++ frontend/my-app/constants/constants.js
@@ -0,0 +1,41 @@
+export const contract_address = "0x41d4ffEB840e117Ff162fFBA1d9f0aa084ae4Df5";
+
+export const contract_abi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
